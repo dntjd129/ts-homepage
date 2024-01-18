51e4e89de3d89a645ec35e3cebc7a132027b997f
dntjd129 <dntjd129@naver.com>
2024-01-18 18:38:29 +0900

    0118

---
+++ hompage/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainPage from "./pages/MainPage";
+import { Reset } from "styled-reset";
+
+function App() {
+  return (
+    <>
+      <Reset />
+      <MainPage />
+    </>
+  );
+}
+
+export default App;
---
+++ hompage/src/pages/MainPage.tsx
@@ -0,0 +1,16 @@
+function MainPage() {
+  return (
+    <>
+      <div>요하이요하이요하이여ㅛㅇ</div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
+    </>
+  );
+}
+
+export default MainPage;
